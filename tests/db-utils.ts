11e556d615689dc920b024d7e4556cfced6d1c5f
yoni-salt <yonatani@salt.security>
2023-11-06 16:22:16 +0200

    fix playwright tests6

--- tests/db-utils.ts
+++ tests/db-utils.ts
@@ -4,6 +4,7 @@ import { type PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 import { UniqueEnforcer } from 'enforce-unique'
 import {prisma} from "#app/utils/db.server.js";
+// import {clearDatabase} from "#prisma/seed.js";
 
 const uniqueUsernameEnforcer = new UniqueEnforcer()
 
@@ -98,20 +99,21 @@ export async function cleanupDb(prisma: PrismaClient) {
 	// prisma.noteComment.deleteMany()
 	// prisma.password.deleteMany()
 	// prisma.verification.deleteMany()
+	//await clearDatabase()
 
-	const tables = await prisma.$queryRaw<
-		{ name: string }[]
-	>`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
-
-	await prisma.$transaction([
-		// Disable FK constraints to avoid relation conflicts during deletion
-		prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`),
-		// Delete all rows from each table, preserving table structures
-		...tables.map(({ name }) =>
-			prisma.$executeRawUnsafe(`DELETE from "${name}"`),
-		),
-		prisma.$executeRawUnsafe(`PRAGMA foreign_keys = ON`),
-	])
+	// const tables = await prisma.$queryRaw<
+	// 	{ name: string }[]
+	// >`SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE '_prisma_migrations';`
+	//
+	// await prisma.$transaction([
+	// 	// Disable FK constraints to avoid relation conflicts during deletion
+	// 	prisma.$executeRawUnsafe(`PRAGMA foreign_keys = OFF`),
+	// 	// Delete all rows from each table, preserving table structures
+	// 	...tables.map(({ name }) =>
+	// 		prisma.$executeRawUnsafe(`DELETE from "${name}"`),
+	// 	),
+	// 	prisma.$executeRawUnsafe(`PRAGMA foreign_keys = ON`),
+	// ])
 }
 
 export type ImageType = Awaited<ReturnType<typeof img>>;
